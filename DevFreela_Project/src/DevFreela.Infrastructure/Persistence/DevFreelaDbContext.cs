9d54eee20d699acf8542fcd6119e89e1dc4fcce4
Ricardo Fulgencio Alves <ricardo.alves@socialbank.com.br>
2022-03-18 16:32:51 -0300

    Criação Infrastructure

---
+++ DevFreela_Project/src/DevFreela.Infrastructure/Persistence/DevFreelaDbContext.cs
@@ -0,0 +1,20 @@
+﻿using DevFreela.Core.Entities;
+
+namespace DevFreela.Infrastructure.Persistence
+{
+    public class DevFreelaDbContext
+    {
+        public DevFreelaDbContext()
+        {
+            Projects = new List<Project>
+            {
+                new Project("Meu projeto ASP.NET Core", "Minha descrição de projeto 1", 1, 1, 10000),
+                new Project("Meu projeto ASP.NET Core", "Minha descrição de projeto 2", 1, 1, 20000),
+                new Project("Meu projeto ASP.NET Core", "Minha descrição de projeto 3", 1, 1, 30000),
+            };
+        }
+        public List<Project> Projects { get; set; }
+        public List<User> Users { get; set; }
+        public List<Skill> Skills { get; set; }
+    }
+}
