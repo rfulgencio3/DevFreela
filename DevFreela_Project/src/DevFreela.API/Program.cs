c2f5212dd0f80e1189acff83f4049a0fa987c5ac
Ricardo Fulgencio Alves <ricardo.alves@socialbank.com.br>
2022-03-21 18:30:53 -0300

    Utilizando MediatR com CQRS

--- DevFreela_Project/src/DevFreela.API/Program.cs
+++ DevFreela_Project/src/DevFreela.API/Program.cs
@@ -1,13 +1,14 @@
-using DevFreela.API.Models;
+using DevFreela.Application.Commands.CreateProject;
 using DevFreela.Application.Services.Implementations;
 using DevFreela.Application.Services.Interfaces;
 using DevFreela.Infrastructure.Persistence;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-IConfiguration Configuration = builder.Configuration;
-builder.Services.Configure<OppeningTimeOption>(Configuration.GetSection("OppeningTime"));
+//IConfiguration Configuration = builder.Configuration;
+//builder.Services.Configure<OppeningTimeOption>(Configuration.GetSection("OppeningTime"));
 
 //var connectionString = Configuration.GetConnectionString("DevFreelaCs");
 
@@ -26,6 +27,9 @@ builder.Services.AddScoped<ISkillService, SkillService>();
 //builder.Services.AddScoped<IProjectService, ProjectService>();
 
 builder.Services.AddControllers();
+
+builder.Services.AddMediatR(typeof(CreateProjectCommand).Assembly);
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
